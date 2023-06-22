d79833c7db39a5da5cf8265f779164d86004525b
OneGulshan <gulshankumar.mailid01@gmail.com>
2023-06-22 21:42:55 +0530

    Add project files.

---
+++ Blazor/Server/Data/ApplicationDBContextcs.cs
@@ -0,0 +1,13 @@
+﻿using Blazor.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blazor.Server.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Developer> Developers { get; set; }
+    }
+}
